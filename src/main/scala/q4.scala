f8a512b4da155c7ba2496dc7bb31fa6d44a089c7
nkudinov <oracle_developer@mail.ru>
2019-10-31 23:17:32 +0300

    add partition

--- src/main/scala/q4.scala
+++
@@ -1,6 +0,0 @@
-object sq4 extends App {
-  def replaceSpaces(str:String):String ={
-    str.flatMap( e => if ( e == ' ') Seq('%','2','0') else Seq(e) )
-  }
-  println(replaceSpaces("Hello worlds"))
-}
