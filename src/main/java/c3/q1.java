1393d23f17abfb8abe2f75fe5430315568c91133
nkudinov <oracle_developer@mail.ru>
2019-11-11 12:15:39 +0300

    add Stack on Node

---
+++ src/main/java/c3/q1.java
@@ -0,0 +1,16 @@
+package c3;
+
+class Stack3{
+    int stackSize;
+    int buffer[];
+    int [] pointer = new int[] {-1,-1,-1};
+
+    public Stack3(int stackSize) {
+        this.stackSize = stackSize;
+        this.buffer = new int[stackSize*3];
+    }
+
+}
+public class q1 {
+
+}
