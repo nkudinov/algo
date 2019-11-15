878d36bcef9fb760c5b82c77685b34a94f4e0267
nkudinov <oracle_developer@mail.ru>
2019-11-15 23:30:26 +0300

    add Stack3

--- src/main/java/c3/q1.java
+++ src/main/java/c3/q1.java
@@ -2,13 +2,71 @@ package c3;
 
 class Stack3 {
 
-    int stackSize;
-    int buffer[];
+    int size;
+    int arr[];
+    int index1;
+    int index2;
+    int index3;
     int [] pointer = new int[] {-1,-1,-1};
 
-    public Stack3(int stackSize) {
-        this.stackSize = stackSize;
-        this.buffer = new int[stackSize*3];
+    public Stack3(int size) {
+        this.size = size;
+        this.arr  = new int[this.size];
+        index1 = -1;
+        index2 = -1;
+        index3 = size;
+    }
+
+    public void shift(int delat){
+
+    }
+
+    public void push(int index, int value){
+        if (index == 0 ){
+           shift(+1);
+           arr[index1++] = value;
+        }
+        else if (index == 1){
+            if (index2 + 1 <= index3){
+                arr[++index2] = value;
+            } else
+                throw new IllegalStateException("stack is full(2)");
+        } else {
+            if (index3 - 1 > index2){
+                arr[--index3] = value;
+            } else
+                throw new IllegalStateException("stack is full (3)");
+        }
+    }
+    public int pop(int index){
+        if (index == 0) {
+            shift(-1);
+            return arr[index1--];
+        } else if ( index == 1 ) {
+            if (index2 >= 0){
+                return arr[index2--];
+            } else
+                throw new IllegalStateException("stack is empty(2)");
+        }
+        else {
+            if (index3 <= size -1 ){
+                return arr[index3++];
+            } else
+                throw new IllegalStateException("stack is empty(3)");
+        }
+    }
+    public void print(){
+        if (index2 >= 0 ) {
+            for (int i = index1 + 1; i <= index2; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+        System.out.println(" ");
+        if (index3 < size) {
+            for (int i = size-1; i >= index3; i--) {
+                System.out.print(arr[i] + " ");
+            }
+        }
     }
 
 }
@@ -65,19 +123,14 @@ class Stack2 {
 }
 public class q1 {
     public static void main(String[] args) {
-      Stack2 stack = new Stack2(10);
-      stack.push(0,1);
-      stack.push(0,2);
-      stack.pop(0);
-      stack.pop(0);
-      stack.push(0,3);
+      Stack3 stack = new Stack3(10);
 
       stack.push(1,1);
-      stack.push(1,2);
-      stack.pop(1);
-      stack.pop(1);
       stack.push(1,1);
-      stack.push(1,2);
+
+
+      stack.push(2,2);
+      stack.push(2,2);
 
       stack.print();
     }
