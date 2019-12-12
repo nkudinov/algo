97df2e4d69206bd78161e3da1934c31670c81411
nkudinov <oracle_developer@mail.ru>
2019-12-12 20:52:27 +0300

    add isPowerOfTwo

--- src/main/java/c5/q4.java
+++ src/main/java/c5/q4.java
@@ -2,6 +2,8 @@ package c5;
 
 public class q4 {
     public static boolean isPowerOfTwo(int n){
+        //x y 1 0 0
+        //x y 0 1 1
         return (n&(n-1))==0;
     }
     public static void main(String[] args) {
