b106a02c9a066e311f5240ba9efb5629e4fbc21f
nkudinov <oracle_developer@mail.ru>
2019-12-14 15:05:30 +0300

    add BloomFilter1

--- src/main/java/gci/BloomFilter1.java
+++ src/main/java/gci/BloomFilter1.java
@@ -23,6 +23,7 @@ public class BloomFilter1 {
         md.update(bytes, 0, bytes.length);
         return Math.abs(new BigInteger(1, md.digest()).intValue()) % (set.length - 1);
     }
+
     private int[] getIndexes(int x){
        int[] ret = new int[32];
        for(int i=0; i < 32; i++){
@@ -31,6 +32,7 @@ public class BloomFilter1 {
        }
        return ret;
     }
+
     public void add(Object o){
        for(int index:getIndexes(o.hashCode())){
            set[index] = 1;
