2d3eee95f420fca81c6201ecfa17b8d3ad4ccaa2
nkudinov <oracle_developer@mail.ru>
2019-12-14 11:13:22 +0300

    add BloomFilter1

--- src/main/java/gci/BloomFilter1.java
+++ src/main/java/gci/BloomFilter1.java
@@ -23,18 +23,35 @@ public class BloomFilter1 {
         md.update(bytes, 0, bytes.length);
         return Math.abs(new BigInteger(1, md.digest()).intValue()) % (set.length - 1);
     }
+    private int[] getIndexes(int x){
+       int[] ret = new int[32];
+       for(int i=0; i < 32; i++){
+           ret[i] = x % (set.length - 1);
+           x = x >> 1;
+       }
+       return ret;
+    }
     public void add(Object o){
-       int index = getHash(o.hashCode());
-       set[index]= 1;
+       for(int index:getIndexes(o.hashCode())){
+           set[index] = 1;
+       }
     }
     public boolean contains(Object o){
-       int index = getHash(o.hashCode());
-       return set[index]== 1;
+        for(int index:getIndexes(o.hashCode())){
+           if (set[index] == 0)
+               return false;
+        }
+        return true;
     }
     public static void main(String[] args) {
        BloomFilter1 bf = new BloomFilter1(1024);
        Integer n1 = 10000;
+       String  s1 = "hello";
        bf.add(n1);
+       bf.add(s1);
        System.out.println(bf.contains(n1));
+       System.out.println(bf.contains(s1));
+        System.out.println(bf.contains(1111));
+        System.out.println(bf.contains("world"));
     }
 }
