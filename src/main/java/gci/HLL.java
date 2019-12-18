f35d36b7c1ca120de3c4a0e1962eadc4b0e2332e
nkudinov <oracle_developer@mail.ru>
2019-12-18 20:34:08 +0300

    add stub for HLL

--- src/main/java/gci/HLL.java
+++ src/main/java/gci/HLL.java
@@ -1,5 +1,25 @@
 package gci;
 
-public class HLL {
+import java.util.Random;
 
+public class HLL {
+    int [] set ;
+    public HLL() {
+      set = new int[4];
+    }
+
+    public void add(Object o ){
+
+    }
+    public int aproxCount(){
+        return 0;
+    }
+    public static void main(String[] args) {
+
+        HLL hll = new HLL();
+        Random rand = new Random();
+        for(int i=0; i< 678001;i++)
+            hll.add(rand.nextInt());
+        System.out.println(hll.aproxCount());
+    }
 }
