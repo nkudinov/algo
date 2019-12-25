fd0a8297c4aaebae926f79ecd0a2bf9a682809ab
nkudinov <oracle_developer@mail.ru>
2019-12-26 01:06:51 +0300

    add q2

---
+++ src/main/java/c7/q2.java
@@ -0,0 +1,12 @@
+package c7;
+
+public class q2 {
+/*
+  L L l
+  R R R
+  - no collision
+  == 2 i/e 1/4
+  There are 2^3 = 8 possible events
+  1 - 1/4 = 3/4
+ */
+}
--- src/main/java/gci/HLL.java
+++ src/main/java/gci/HLL.java
@@ -4,47 +4,51 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class HLL {
-    byte [] set ;
+
+    int [] set ;
 
     public HLL() {
-        set = new byte[128];
+        set = new int[256];
     }
 
-    private byte getBucket(int x){
-       return (byte)( x >>> 24);
+    private int getBucket(int x){
+      return x&0xFF;
     }
-    private byte getZeros(int x){
+
+    private byte getZerosNum(int x){
+        x = x >> 9;
         byte ret = 0;
         for(int i =0; i< 20;i++){
-            if ( (x&0x1) == 1)
+            if ( (x&0x1) == 0)
                 ret++;
             else
                break;
-            x = x >>1;
+            x = x >> 1;
         }
         return ret;
     }
+
     public void add(int x){
-       byte bucket = getBucket(x);
-       byte zeros  = getZeros(x);
-       set[bucket] = (byte) Math.max( set[bucket], zeros);
+       int bucket  = getBucket(x);
+       byte zeros  = getZerosNum(x);
+       set[bucket] = Math.max( set[bucket], zeros);
     }
 
     public Long aproxCount(){
         double sum = 0;
-        for(int i=0;i < set.length;i++)
+        for(int i=0;i < set.length; i++)
             sum += Math.pow(2,-1*set[i]);
-        return  Math.round(0.7213*set.length*set.length*1/sum);
+        return  Math.round(0.7182725932495459*1/sum*set.length*set.length);
     }
 
     public static void main(String[] args) {
 
         HLL hll = new HLL();
         Random rand = new Random();
-        for( int i=0; i < 1_000_000; i++)
-             hll.add( rand.nextInt( Integer.MAX_VALUE));
+        for( int i=0; i < 20_000_000; i++)
+             hll.add( rand.nextInt());
         System.out.println( hll.aproxCount());
-        System.out.println(Arrays.toString(hll.set));
+        System.out.println( Arrays.toString(hll.set));
     }
 
 }
