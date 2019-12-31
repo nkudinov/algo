5af0c97aae3d71df8ec72dd7738fed04d8259f96
nkudinov <oracle_developer@mail.ru>
2019-12-31 21:28:36 +0300

    use y == k*x+b formula

--- src/main/java/c7/q3.java
+++ src/main/java/c7/q3.java
@@ -3,22 +3,25 @@ package c7;
 public class q3 {
     public static class Line {
         private static double epsilon = 0.000001;
-        private double slope ;
-        private double yintersept;
+        private double k ;
+        private double b;
 
-        public Line(double slope, double yintersept) {
-            this.slope = slope;
-            this.yintersept = yintersept;
+        public Line(double k, double b) {
+            this.k = k;
+            this.b = b;
         }
         public boolean intersect(Line line2){
-           return Math.abs(this.slope - line2.slope) >epsilon || Math.abs(line2.yintersept -this.yintersept) < epsilon;
+           return Math.abs(this.k - line2.k) > epsilon || Math.abs(line2.b -this.b) < epsilon;
         }
     }
 
     public static void main(String[] args) {
        Line l1 = new Line(1,1);
        Line l2 = new Line(2,2);
+       Line l3 = new Line(1,2);
+
        System.out.println(l1.intersect(l2));
+       System.out.println(l1.intersect(l3));
     }
 
 }
