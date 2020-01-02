d5de6ef965d2044d1f8eb1207f997e4c929b58cf
nkudinov <oracle_developer@mail.ru>
2020-01-02 21:07:52 +0300

    add multiplication

--- src/main/java/c7/q4.java
+++ src/main/java/c7/q4.java
@@ -1,6 +1,7 @@
 package c7;
 
 public class q4 {
+
     public static int negate(int a){
         int ret =0;
         int d = a > 0?-1:1;
@@ -10,11 +11,23 @@ public class q4 {
         }
         return ret;
     }
+
     public static int minus(int a, int b){
         return a + negate(b);
+    }
+    public static int multiplication(int a, int b){
+        int ret =0;
+        for(int i=1;i<=Math.abs(b);i++){
+            ret += a;
+        }
+        if (b < 0)
+            ret = negate(ret);
+        return ret;
     }
     public static void main(String[] args) {
         System.out.println(negate(10));
         System.out.println(minus(1,10));
+        System.out.println(multiplication(2,-3));
     }
+
 }
