ed6ee52dc8fdd685487f368ad925b1c2c44a3e87
nkudinov <oracle_developer@mail.ru>
2020-03-29 14:37:19 +0300

    add lengthOfLongestSubstring3

--- src/main/java/lc/q3.java
+++ src/main/java/lc/q3.java
@@ -1,5 +1,6 @@
 package lc;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -42,22 +43,19 @@ public class q3 {
     }
     public static int lengthOfLongestSubstring3(String s) {
         int ret = 0;
-        int i = 0;
-        int j = 0;
-        Set<Character> set = new HashSet<>();
-        while( i < s.length() && j < s.length()){
-            if ( !set.contains(s.charAt(j))){
-                set.add(s.charAt(j));
-                j++;
-                ret = Math.max(ret, j - i);
-            } else {
-                set.remove(s.charAt(i));
-                i++;
-            }
+        Map<Character, Integer> m = new HashMap<>();
+        for(int i = 0, j = 0; j < s.length();j++){
+          if (m.containsKey(s.charAt(j))){
+              i = Math.max(i, m.get(s.charAt(j)));
+          }
+          m.put( s.charAt(j), j + 1);
+          ret = Math.max(ret, j - i + 1);
         }
         return ret;
     }
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring2("abcabcbb"));
+       // System.out.println(lengthOfLongestSubstring3("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring3( "abba"));
+
     }
 }
