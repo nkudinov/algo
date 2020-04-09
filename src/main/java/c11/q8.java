10c6117739cd99f4b6e8a3a15f37ed64435b9821
nkudinov <oracle_developer@mail.ru>
2020-04-09 11:27:24 +0300

    add Ranker

--- src/main/java/c11/q8.java
+++ src/main/java/c11/q8.java
@@ -1,11 +1,32 @@
 package c11;
 
+import java.util.NavigableSet;
 import java.util.TreeMap;
 
 public class q8 {
+    static class Ranker {
+        static TreeMap<Integer,Integer> map = new TreeMap<>();
+        public void track(int x) {
+            Integer v = map.get(x);
+            map.put(x , v ==null?1:v+1);
+        }
+        public int getRank(int x){
+            int ret = 0;
+            for(Integer k:map.navigableKeySet()){
+               if (k < x)
+                   ret++;
+            }
+            return ret;
+        }
+    }
     public static void main(String[] args) {
-        TreeMap<Integer,Integer> m = new TreeMap<>();
-
+        Ranker r = new Ranker();
+        r.track(1);
+        r.track(0);
+        r.track(3);
+        System.out.println(r.getRank(1));
+        System.out.println(r.getRank(0));
+        System.out.println(r.getRank(3));
     }
 
 }
