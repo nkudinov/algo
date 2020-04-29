51b9dcb905edea362df60af6ab3804f0a5c6f1af
nkudinov <oracle_developer@mail.ru>
2020-04-29 19:44:39 +0300

    add getTrailingZerosOfFactorial

---
+++ src/main/java/c17/q3.java
@@ -0,0 +1,20 @@
+package c17;
+
+public class q3 {
+    static public int getTrailingZerosOfFactorial(int n){
+        // 100!= 2*3...25...100
+        int d = 1;
+        int c = 0;
+        while( n/d >0 ){
+           d = d*5;
+           c += n/d;
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getTrailingZerosOfFactorial(100));
+        System.out.println(getTrailingZerosOfFactorial(5));
+        System.out.println(getTrailingZerosOfFactorial(10));
+    }
+}
