5cf13cdd192143f73a2bec55a99d3f3be680bc62
nkudinov <oracle_developer@mail.ru>
2020-07-17 13:43:31 +0300

    Radix sort

---
+++ src/main/java/lc/July2020/d17.java
@@ -0,0 +1,10 @@
+package lc.July2020;
+
+public class d17 {
+    public int[] topKFrequent(int[] nums, int k) {
+       return null;
+    }
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/sort/Radix.java
+++ src/main/java/sort/Radix.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 public class Radix {
     static void countSort(int[] arr, int exp) {
          int n = arr.length;
-
          int[] count = new int[10];
          int[] output = new int[arr.length];
 
@@ -19,15 +18,14 @@ public class Radix {
              output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
              count[ (arr[i]/exp)%10 ]--;
          }
-        for (int i = 0; i < n; i++)
-            arr[i] = output[i];
+         for (int i = 0; i < n; i++)
+             arr[i] = output[i];
     }
     static void sort(int[] arr){
         int m = Arrays.stream(arr).max().getAsInt();
         for(int exp = 1; m/exp > 0; exp *= 10){
             countSort(arr, exp);
         }
-
     }
     public static void main(String[] args) {
        int[] arr = new int[]{101,2,1,803};
