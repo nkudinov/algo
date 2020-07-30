89de8227c86941f7a41e423bc1bdbb49c9aa36dc
nkudinov <oracle_developer@mail.ru>
2020-07-30 15:57:28 +0300

    join

--- src/main/java/rest/Join.java
+++ src/main/java/rest/Join.java
@@ -7,55 +7,68 @@ import java.util.Iterator;
 
 
 public class Join {
-    private static  Integer internalNext(Iterator<Integer> iter){
-        if (iter.hasNext()){
-            return iter.next();
+    static class JoinIntegerIterator implements Iterator<Integer>{
+        Integer leftRow;
+        Integer righRow;
+        Iterator<Integer> left;
+        Iterator<Integer> right;
+        private Integer AdvancedNext(Iterator<Integer> iter){
+            if (iter.hasNext()){
+                return iter.next();
+            }
+            return null;
         }
-        return null;
+        private boolean findNextInnerJoinRows(){
+          if ( leftRow == null){
+              leftRow = AdvancedNext(left);
+          }
+          righRow = AdvancedNext(right);
+          if (leftRow == null){
+              return false;
+          } else if (righRow == null){
+              return false;
+          } else {
+              do {
+                 int comp = leftRow.compareTo(righRow);
+                 if (comp < 0){
+                     leftRow = AdvancedNext(left);
+                 } else if (comp >0){
+                     righRow = AdvancedNext(right);
+                 }
+              } while ( leftRow != null && righRow != null && leftRow != righRow);
+              if ( left!= null && right!= null & left == right){
+                  return true;
+              }
+              return false;
+          }
+        }
+        public JoinIntegerIterator(Iterator<Integer> left, Iterator<Integer> right) {
+            this.left = left;
+            this.right = right;
+            findNextInnerJoinRows();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return leftRow != null && righRow != null;
+        }
+
+        @Override
+        public Integer next() {
+            Integer ret = leftRow;
+            findNextInnerJoinRows();
+            return ret;
+        }
     }
+
     public static Iterator<Integer> join(Iterator<Integer> left, Iterator<Integer> right){
 
-          return new Iterator<Integer> () {
+          return new JoinIntegerIterator(left, right);
 
-
-
-              Integer leftRow = left.next();
-              Integer righRow = right.next();
-              @Override
-              public boolean hasNext() {
-                  return leftRow != null & righRow!= null;
-              }
-
-              @Override
-              public Integer next() {
-                  Integer ret = null;
-                  if ( leftRow == righRow) {
-
-                      ret = leftRow;
-
-
-                  } else if (leftRow > righRow){
-                      righRow = internalNext( right);
-                      while ( leftRow > righRow ){
-                          righRow = internalNext( right);
-                      }
-                      ret = leftRow;
-                      righRow = internalNext( right);
-                  } else {
-                      leftRow = internalNext(left);
-                      while ( leftRow < righRow ){
-                          leftRow = internalNext(left);
-                      }
-                      ret = leftRow;
-                      righRow = internalNext( right);
-                  }
-                  return ret;
-              }
-          };
     }
     public static void main(String[] args) {
-        Iterator<Integer> l = Arrays.stream(new int[]{1,3}).iterator();
-        Iterator<Integer> r = Arrays.stream(new int[]{1,2,3}).iterator();
+        Iterator<Integer> l = Arrays.stream(new int[]{1,1}).iterator();
+        Iterator<Integer> r = Arrays.stream(new int[]{1,2,3,4}).iterator();
         Iterator<Integer> o = join(l, r);
         while(o.hasNext()){
             System.out.println(o.next());
