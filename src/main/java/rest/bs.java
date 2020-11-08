141ad3eaaaeec59df4e35513dafa581ca9ec0df5
nkudinov <nikolay.a.kudinov@gmail.com>
2020-11-08 12:31:12 +0300

    some algorithms

---
+++ src/main/java/rest/bs.java
@@ -0,0 +1,12 @@
+package rest;
+
+import java.util.Arrays;
+
+public class bs {
+
+    public static void main(String[] args) {
+      int[] arr = new int[]{1,4,6};
+        System.out.println(Arrays.toString("abc dff ggg".split(" ",2)));
+    }
+
+}
