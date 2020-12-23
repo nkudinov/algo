a176941f275a5bc8ce686ab33300f814db730b4e
nkudinov <nikolay.a.kudinov@gmail.com>
2020-12-23 20:06:43 +0300

    some algorithms

--- src/main/java/l100/q2.java
+++ src/main/java/l100/q2.java
@@ -1,5 +1,7 @@
 package l100;
 
+import java.util.Stack;
+
 public class q2 {
     public static int largestRectangleArea(int[] heights) {
         int ans = 0;
@@ -14,8 +16,27 @@ public class q2 {
         }
         return ans;
     }
+    public static int largestRectangleArea2(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int ans = 0;
+        for( int i = 0 ; i < heights.length; i++){
+            while(stack.peek()!=-1 && heights[stack.peek()] > heights[i]){
+                int h = heights[ stack.pop()];
+                int w = i - stack.peek() - 1;
+                ans = Math.max( ans, h*w);
+            }
+            stack.push(i);
+        }
+        while(stack.peek()!=-1){
+            int h = heights[ stack.pop()];
+            int w = heights.length - stack.peek() - 1;
+            ans = Math.max( ans, h*w);
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         int[] arr = new int[]{2,1,5,6,2,3};
-        System.out.println(largestRectangleArea(arr));
+        System.out.println(largestRectangleArea2(arr));
     }
 }
