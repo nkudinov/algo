0284570e40305a54820ed7082c1d98965b9c2de4
nkudinov <nikolay.a.kudinov@gmail.com>
2021-02-05 18:37:13 +0300

    some algorithms

--- src/main/java/l100/q29.java
+++ src/main/java/l100/q29.java
@@ -1,10 +1,12 @@
 package l100;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class q29 {
-    public static List<String> letterCasePermutation(String s) {
+    public static List<String> letterCasePermutation1(String s) {
         List<StringBuilder> tmp = new ArrayList<>();
         tmp.add(new StringBuilder());
         for(char ch:s.toCharArray()){
@@ -29,6 +31,24 @@ public class q29 {
         }
         return ans;
     }
+    public static List<String> letterCasePermutation(String S) {
+        int N = S.length();
+        Set<String> set = new HashSet<>();
+        for(int i = 0; i < (1<<N); i++){
+            StringBuilder sb = new StringBuilder();
+            for(int j = 0; j < N; j++){
+                char ch = S.charAt(j);
+                if ( (i & (1<<j)) == 0){
+                    sb.append(Character.toLowerCase(ch));
+                }else {
+                    sb.append(Character.toUpperCase(ch));
+                }
+            }
+            set.add(sb.toString());
+        }
+        return new ArrayList<>(set);
+    }
+
     public static void main(String[] args) {
        for(String s:letterCasePermutation("a1b2")){
            System.out.println(s);
