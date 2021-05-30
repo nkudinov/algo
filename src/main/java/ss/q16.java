810e085c7e2a9af55c519a86e37cd31295a364f4
nkudinov <nikolay.a.kudinov@gmail.com>
2021-05-30 15:19:26 +0200

    some algorithms

--- src/main/java/ss/q16.java
+++ src/main/java/ss/q16.java
@@ -1,8 +1,6 @@
 package ss;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class q16 {
     public int leastInterval(char[] tasks, int n) {
