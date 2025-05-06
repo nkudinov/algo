c43abeaeeba1be20b0214fc499e39cdbd4b2d431
Nikolay Kudinov <nikolay.a.kudinov@gmail.com>
2025-05-06 11:53:37 +0200

    add snake 5

--- src/main/java/Tetris6.java
+++ src/main/java/Tetris6.java
@@ -3,66 +3,97 @@ import java.util.Scanner;
 
 public class Tetris6 {
     public static void main(String[] args) {
-        int[][] board = new int[10][20];
+        int[][] board = new int[20][10]; // rows x cols
         int[][] figure = new int[][]{{1, 1}, {1, 1}};
         char op = '?';
         Scanner scanner = new Scanner(System.in);
         int col = 0;
+
         while (op != 'x') {
             display(col, board, figure);
-            System.out.println("Please enter: x- exit, l - left, r - right, d - drop:");
+            System.out.println("Please enter: x - exit, l - left, r - right, d - drop:");
             op = scanner.nextLine().charAt(0);
             switch (op) {
                 case 'l' -> col = Math.max(col - 1, 0);
-                case 'r' -> col = Math.min(col + 1, board.length - figure.length);
-                case 'd' -> dropFigure(board, figure);
+                case 'r' -> col = Math.min(col + 1, board[0].length - figure[0].length);
+                case 'd' -> {
+                    dropFigure(col, board, figure);
+                    col = 0;
+                }
             }
         }
+        scanner.close();
     }
 
     private static void display(int col, int[][] board, int[][] figure) {
         char[][] display = new char[board.length][board[0].length];
         for (int r = 0; r < board.length; r++) {
-            Arrays.fill(display[r],'.');
+            Arrays.fill(display[r], '.');
             for (int c = 0; c < board[0].length; c++) {
-                if (board[r][c] == 0) {
-                    continue;
+                if (board[r][c] == 1) {
+                    display[r][c] = 'X';
                 }
-                display[r][c] = 'X';
             }
         }
+
         for (int r = 0; r < figure.length; r++) {
             for (int c = 0; c < figure[0].length; c++) {
-                if (figure[r][c] == 0) {
-                    continue;
+                if (figure[r][c] == 1) {
+                    int boardRow = r;
+                    int boardCol = col + c;
+                    if (boardRow < board.length && boardCol < board[0].length) {
+                        display[boardRow][boardCol] = 'X';
+                    }
                 }
-                 int boardRow = r;
-                 int boardCol = col + c;
-                display[boardRow][boardCol] = 'X';
             }
         }
-        for(int r=  0; r < display.length; r++) {
-            StringBuilder sb = new StringBuilder();
-            for(int c = 0; c < display[0].length;c++) {
-                sb.append(' ').append(display[r][c]);
+
+        for (char[] row : display) {
+            for (char cell : row) {
+                System.out.print(" " + cell);
             }
-            System.out.println(sb.toString());
+            System.out.println();
         }
     }
 
     private static void dropFigure(int col, int[][] board, int[][] figure) {
         int row = 0;
-        while ( canPlace(row, col, board, figure)) {
+        while (canPlace(row, col, board, figure)) {
             row++;
         }
         row--;
-        placeFigure(row, col, board, figure);
+        if (row >= 0) {
+            placeFigure(row, col, board, figure);
+        }
     }
 
     private static void placeFigure(int row, int col, int[][] board, int[][] figure) {
-
+        for (int r = 0; r < figure.length; r++) {
+            for (int c = 0; c < figure[0].length; c++) {
+                if (figure[r][c] == 1) {
+                    board[row + r][col + c] = 1;
+                }
+            }
+        }
     }
 
     private static boolean canPlace(int row, int col, int[][] board, int[][] figure) {
+        for (int r = 0; r < figure.length; r++) {
+            for (int c = 0; c < figure[0].length; c++) {
+                if (figure[r][c] == 1) {
+                    int boardRow = row + r;
+                    int boardCol = col + c;
+
+                    if (boardRow >= board.length || boardCol < 0 || boardCol >= board[0].length) {
+                        return false;
+                    }
+
+                    if (board[boardRow][boardCol] == 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
