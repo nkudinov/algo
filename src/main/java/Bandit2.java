7ca2c39af02d51b9323c7807ce148afa0931b7cd
nikolay-kudinov_zse <nikolay.a.kudinov@gmail.com>
2025-07-14 10:42:11 +0200

    add decayRate

--- src/main/java/Bandit2.java
+++ src/main/java/Bandit2.java
@@ -3,14 +3,15 @@ import java.util.Random;
 public class Bandit2 {
 
     public static void main(String[] args) {
-        double[] trueProbability = new double[]{0.1, 0.99, 0.1};
+        double[] trueProbability = new double[]{0.1, 0.7, 0.1};
         double[] guessProbability = new double[3];
         int[] counts = new int[3];
         double epsilon = 0.01;
         Random rand = new Random();
-
-        for (int i = 0; i < 10000; i++) {
+        double decayRate = 0.9999999;
+        for (int i = 0; i < 100000; i++) {
             int index;
+            epsilon *= decayRate;
             if (rand.nextDouble() < epsilon) {
                 index = rand.nextInt(3); // Explore
             } else {
